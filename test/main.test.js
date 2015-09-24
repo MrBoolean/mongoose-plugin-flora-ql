db084a5f3925c0c0672fa5dcfd58b34fdd2fcc49
MrBoolean <marcandrebinder@gmail.com>
2015-09-24 14:00:18 +0200

    Rename

--- test/main.test.js
+++ test/main.test.js
@@ -1,32 +1,32 @@
 var expect = require('chai').expect;
-var parse  = require('../');
+var fqlm   = require('../');
 
-describe('flora-ql-mongodb', function fqlm() {
+describe('flora-ql-mongodb', function test() {
   it('!= to $ne', function ne() {
-    expect(parse('firstname!="Jon"')).to.eql({ firstname: { $ne: 'Jon' } });
+    expect(fqlm('firstname!="Jon"')).to.eql({ firstname: { $ne: 'Jon' } });
   });
 
   it('<= to $lte', function lte() {
-    expect(parse('age<=1337')).to.eql({ age: { $lte: 1337 } });
+    expect(fqlm('age<=1337')).to.eql({ age: { $lte: 1337 } });
   });
 
   it('>= to $gte', function gte() {
-    expect(parse('age>=1337')).to.eql({ age: { $gte: 1337 } });
+    expect(fqlm('age>=1337')).to.eql({ age: { $gte: 1337 } });
   });
 
   it('= to attribute: value', function equal() {
-    expect(parse('age=1337')).to.eql({ age: 1337 });
+    expect(fqlm('age=1337')).to.eql({ age: 1337 });
   });
 
   it('= (multiple values) to $in', function equalIn() {
-    expect(parse('rights="admin","chuckNorris"')).to.eql({ rights: { $in: ['admin', 'chuckNorris'] } });
+    expect(fqlm('rights="admin","chuckNorris"')).to.eql({ rights: { $in: ['admin', 'chuckNorris'] } });
   });
 
   it('< to $lt', function lt() {
-    expect(parse('age<1337')).to.eql({ age: { $lt: 1337 } });
+    expect(fqlm('age<1337')).to.eql({ age: { $lt: 1337 } });
   });
 
   it('> to $gt', function gt() {
-    expect(parse('age>1337')).to.eql({ age: { $gt: 1337 } });
+    expect(fqlm('age>1337')).to.eql({ age: { $gt: 1337 } });
   });
 });
